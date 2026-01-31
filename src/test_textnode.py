e6381032f1931205f51ca7451e1cbe3b25a9fff0
Ex0ticButt3rs <mearchie@alum.mit.edu>
2026-01-31 01:49:12 -0500

    added way to convert textnode to htmlnode as leafnode

--- src/test_textnode.py
+++ src/test_textnode.py
@@ -1,5 +1,5 @@
 import unittest
-from textnode import TextNode, TextType
+from textnode import TextNode, TextType, text_node_to_html_node
 
 class TestTextNode(unittest.TestCase):
     def test_eq(self):
@@ -14,6 +14,12 @@ class TestTextNode(unittest.TestCase):
         node5 = TextNode("This is a text node", TextType.CODE, None)
         node6 = TextNode("This is a text node", TextType.CODE)
         self.assertEqual(node5, node6)
+
+    def test_text(self):
+        node = TextNode("This is a text node", TextType.TEXT)
+        html_node = text_node_to_html_node(node)
+        self.assertEqual(html_node.tag, None)
+        self.assertEqual(html_node.value, "This is a text node")
 
 if __name__ == "__main__":
     unittest.main()
