f2223331a6e137d30f3f1f94ecec33a391264cfc
Ex0ticButt3rs <mearchie@alum.mit.edu>
2026-02-02 17:37:27 -0500

    added ability to copy static files from one directory into another directory that is created within the program

--- src/main.py
+++ src/main.py
@@ -1,7 +1,30 @@
 from textnode import TextNode, TextType
+import os
+import shutil
 
 def main():
-    new_text_node = TextNode("hi", TextType.BOLD_TEXT)
-    print(new_text_node)
+    source = "static"
+    destination = "public"
+    
+    if os.path.exists(destination):    
+        shutil.rmtree(destination)
+    os.mkdir(destination)
+    copy_static(source, destination)    
 
-main()
+def copy_static(source, destination):
+    if not os.path.exists(source):
+        raise Exception("static folder is missing or misnamed")
+    if not os.path.exists(destination):
+        raise Exception("provided destination isn't an existing path")
+    
+    for i in os.listdir(source):
+        entry_path = os.path.join(source, i)
+        if os.path.isfile(entry_path):
+            shutil.copy(entry_path, destination)
+        else:
+            dest_directory = os.path.join(destination, i)
+            os.mkdir(dest_directory)
+            copy_static(entry_path, dest_directory)
+
+if __name__ == "__main__":
+    main()
