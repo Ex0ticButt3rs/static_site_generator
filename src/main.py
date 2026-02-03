ffaa8ba8b661c0b052261e3d87f1390f704fd559
Ex0ticButt3rs <mearchie@alum.mit.edu>
2026-02-02 22:15:55 -0500

    deploying to internet

--- src/main.py
+++ src/main.py
@@ -5,20 +5,25 @@ import re
 from splitblocks import markdown_to_html_node
 from parentnode import ParentNode
 from pathlib import Path
+import sys
 
 def main():
+    if len(sys.argv) > 1:
+        basepath = sys.argv[1]
+    else:
+        basepath = "/"    
     source = "static"
-    destination = "public"
+    destination = "docs"
     if os.path.exists(destination):    
         shutil.rmtree(destination)
     os.mkdir(destination)
     copy_static(source, destination)
     dir_path_content = Path("content")
     template_path = "template.html"
-    dest_dir_path = Path("public")
-    generate_pages_recursive(dir_path_content, template_path, dest_dir_path)
+    dest_dir_path = Path("docs")
+    generate_pages_recursive(dir_path_content, template_path, dest_dir_path, basepath)
 
-def generate_page(from_path, template_path, dest_path):
+def generate_page(from_path, template_path, dest_path, basepath):
     print(f"Generating page from {from_path} to {dest_path} using {template_path}")
     with open(from_path, mode='r') as opened_markdown_object:
         markdown_read_string = opened_markdown_object.read()
@@ -27,25 +32,24 @@ def generate_page(from_path, template_path, dest_path):
         page_title = extract_title(markdown_read_string)
     with open(template_path, mode='r') as opened_template_object:
         template_read_string = opened_template_object.read()
-        updated_template_string = template_read_string.replace("{{ Title }}", page_title).replace("{{ Content }}", markdown_to_html_result)
+        updated_template_string = template_read_string.replace("{{ Title }}", page_title).replace("{{ Content }}", markdown_to_html_result).replace('href="/', f'href="{basepath}').replace('src="/', f'src="{basepath}')
         dir_name = os.path.dirname(dest_path)
         if dest_path and not os.path.exists(dir_name):
             os.makedirs(dir_name)
         with open(dest_path, mode='w') as opened_destination_object:
             opened_destination_object.write(updated_template_string)
 
-def generate_pages_recursive(dir_path_content, template_path, dest_dir_path):
+def generate_pages_recursive(dir_path_content, template_path, dest_dir_path, basepath):
     dir_path_content = Path(dir_path_content)
     dest_dir_path = Path(dest_dir_path)
-
     for entry in dir_path_content.iterdir():
         if entry.is_dir():
-            generate_pages_recursive(entry, template_path, dest_dir_path)
+            generate_pages_recursive(entry, template_path, dest_dir_path, basepath)
         if entry.is_file() and entry.suffix == ".md":
             relative = entry.relative_to(Path("content"))
             dest_path = dest_dir_path / relative
             dest_path = dest_path.with_suffix(".html")
-            generate_page(str(entry), template_path, str(dest_path))
+            generate_page(str(entry), template_path, str(dest_path), basepath)
 
 def extract_title(markdown):
     substrings_list = markdown.split("\n")
