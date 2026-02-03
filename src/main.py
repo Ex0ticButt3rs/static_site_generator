f10dbef35cb22280f6e305ac14477aa654bb1a0a
Ex0ticButt3rs <mearchie@alum.mit.edu>
2026-02-02 21:42:10 -0500

    added ability to generate pages recursively

--- src/main.py
+++ src/main.py
@@ -4,6 +4,7 @@ import shutil
 import re
 from splitblocks import markdown_to_html_node
 from parentnode import ParentNode
+from pathlib import Path
 
 def main():
     source = "static"
@@ -12,33 +13,10 @@ def main():
         shutil.rmtree(destination)
     os.mkdir(destination)
     copy_static(source, destination)
-    from_path = "content/index.md"
+    dir_path_content = Path("content")
     template_path = "template.html"
-    dest_path = "public/index.html"
-    generate_page(from_path, template_path, dest_path)
-
-def extract_title(markdown):
-    substrings_list = markdown.split("\n")
-    for line in substrings_list:
-        if re.match(r"^#\s+.+", line):
-            text = line.lstrip("#").strip()
-            return text
-    raise Exception("no h1 header line exists")
-
-def copy_static(source, destination):
-    if not os.path.exists(source):
-        raise Exception("static folder is missing or misnamed")
-    if not os.path.exists(destination):
-        raise Exception("provided destination isn't an existing path")
-    
-    for i in os.listdir(source):
-        entry_path = os.path.join(source, i)
-        if os.path.isfile(entry_path):
-            shutil.copy(entry_path, destination)
-        else:
-            dest_directory = os.path.join(destination, i)
-            os.mkdir(dest_directory)
-            copy_static(entry_path, dest_directory)
+    dest_dir_path = Path("public")
+    generate_pages_recursive(dir_path_content, template_path, dest_dir_path)
 
 def generate_page(from_path, template_path, dest_path):
     print(f"Generating page from {from_path} to {dest_path} using {template_path}")
@@ -55,6 +33,39 @@ def generate_page(from_path, template_path, dest_path):
             os.makedirs(dir_name)
         with open(dest_path, mode='w') as opened_destination_object:
             opened_destination_object.write(updated_template_string)
+
+def generate_pages_recursive(dir_path_content, template_path, dest_dir_path):
+    dir_path_content = Path(dir_path_content)
+    dest_dir_path = Path(dest_dir_path)
+
+    for entry in dir_path_content.iterdir():
+        if entry.is_dir():
+            generate_pages_recursive(entry, template_path, dest_dir_path)
+        if entry.is_file() and entry.suffix == ".md":
+            relative = entry.relative_to(Path("content"))
+            dest_path = dest_dir_path / relative
+            dest_path = dest_path.with_suffix(".html")
+            generate_page(str(entry), template_path, str(dest_path))
+
+def extract_title(markdown):
+    substrings_list = markdown.split("\n")
+    for line in substrings_list:
+        if re.match(r"^#\s+.+", line):
+            text = line.lstrip("#").strip()
+            return text
+    raise Exception("no h1 header line exists")
+
+def copy_static(source, destination):
+    if not os.path.exists(source):
+        raise Exception("static folder is missing or misnamed")
+    for i in os.listdir(source):
+        entry_path = os.path.join(source, i)
+        if os.path.isfile(entry_path):
+            shutil.copy(entry_path, destination)
+        else:
+            dest_directory = os.path.join(destination, i)
+            os.mkdir(dest_directory)
+            copy_static(entry_path, dest_directory)
 
 if __name__ == "__main__":
     main()
